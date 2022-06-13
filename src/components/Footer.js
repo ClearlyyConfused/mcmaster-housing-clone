a6f99faf1c3721bd803423e31e7b5953998153e6
Louis Doan <louisdoan9@gmail.com>
2022-06-12 21:05:53 -0400

    Adjust footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,40 @@
 import './Footer.css';
 import Logo from '../images/McMasterBrighterWorldLogo.png';
+import Instagram from '../images/instagram.svg';
+import Twitter from '../images/twitter.svg';
+import Facebook from '../images/facebook.svg';
+import Youtube from '../images/youtube.svg';
+import Linkedin from '../images/linkedin.svg';
 
 function Footer() {
 	return (
 		<footer className="Footer">
 			<img src={Logo} alt="Logo" />
 			<section className="section1">
-				<a href="">CONTACT</a>
-				<a href="">TERMS AND CONDITIONS</a>
-				<a href="">PRIVACY POLICY</a>
+				<div className="divider"></div>
+				<div className="section1Content">
+					<a href="">CONTACT</a>
+					<a href="">TERMS AND CONDITIONS</a>
+					<a href="">PRIVACY POLICY</a>
+				</div>
+				<div className="divider"></div>
 			</section>
 			<section className="section2">
-				<a href="">INSTAGRAM</a>
-				<a href="">TWITTER</a>
-				<a href="">FACEBOOK</a>
-				<a href="">YOUTUBE</a>
-				<a href="">LINKEDIN</a>
+				<a href="">
+					<img src={Instagram} alt="Instagram" />
+				</a>
+				<a href="">
+					<img src={Twitter} alt="Twitter" />
+				</a>
+				<a href="">
+					<img src={Facebook} alt="Facebook" />
+				</a>
+				<a href="">
+					<img src={Youtube} alt="Youtube" />
+				</a>
+				<a href="">
+					<img src={Linkedin} alt="Linkedin" />
+				</a>
 			</section>
 			<section className="section3">
 				<a href="">1280 Main Street West  Hamilton, Ontario L8S 3L8</a>
