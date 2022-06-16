96b83fa499f6ef9f8f15b26302a1308baa43a003
Louis Doan <louisdoan9@gmail.com>
2022-06-16 18:26:49 -0400

    Add animation to call to action banner

--- src/components/Homepage/CallToAction.js
+++ src/components/Homepage/CallToAction.js
@@ -3,8 +3,10 @@ import { callToActionInfo } from '../Data';
 
 function CallToAction() {
 	const [currentDisplay, setCurrentDisplay] = useState(0);
+	const [x, setX] = useState(0);
 
 	function changeDisplayForward() {
+		setX(0);
 		if (currentDisplay + 1 === callToActionInfo.length) {
 			setCurrentDisplay(0);
 		} else {
@@ -13,6 +15,7 @@ function CallToAction() {
 	}
 
 	function changeDisplayBackwards() {
+		setX(1);
 		if (currentDisplay - 1 === -1) {
 			setCurrentDisplay(callToActionInfo.length - 1);
 		} else {
@@ -21,24 +24,112 @@ function CallToAction() {
 	}
 
 	useEffect(() => {
-		const timer = setInterval(changeDisplayForward, 5000);
-
-		return () => {
-			clearInterval(timer);
-		};
+		// const timer = setInterval(changeDisplayForward, 10000);
+		// return () => {
+		//clearInterval(timer);
+		//};
 	});
 
 	return (
-		<section className="CallToAction">
-			<button onClick={changeDisplayBackwards}>&lt;</button>
-			<div className="CallToActionInfo">
-				<h3>{callToActionInfo[currentDisplay].title}</h3>
-				<p>{callToActionInfo[currentDisplay].description}</p>
-				<button className="ContactBtn">{callToActionInfo[currentDisplay].contact}</button>
-			</div>
-			<button onClick={changeDisplayForward}>&gt;</button>
-		</section>
+		<div>
+			{callToActionInfo.map((info, index) => {
+				return infoDisplayFirst(
+					info,
+					currentDisplay,
+					index,
+					changeDisplayBackwards,
+					changeDisplayForward,
+					x
+				);
+			})}
+			{callToActionInfo.map((info, index) => {
+				return infoDisplay(
+					info,
+					currentDisplay,
+					index,
+					changeDisplayBackwards,
+					changeDisplayForward,
+					x
+				);
+			})}
+		</div>
 	);
+}
+
+function infoDisplayFirst(
+	info,
+	currentDisplay,
+	index,
+	changeDisplayBackwards,
+	changeDisplayForward,
+	x
+) {
+	if (currentDisplay === index) {
+		if (x === 0) {
+			return (
+				<section className="CallToActionF">
+					<button onClick={changeDisplayBackwards}>&lt;</button>
+					<div className="CallToActionInfo">
+						<h3>{info.title}</h3>
+						<p>{info.description}</p>
+						<button className="ContactBtn">{info.contact}</button>
+					</div>
+					<button onClick={changeDisplayForward}>&gt;</button>
+				</section>
+			);
+		} else {
+			return (
+				<section className="CallToActionB">
+					<button onClick={changeDisplayBackwards}>&lt;</button>
+					<div className="CallToActionInfo">
+						<h3>{info.title}</h3>
+						<p>{info.description}</p>
+						<button className="ContactBtn">{info.contact}</button>
+					</div>
+					<button onClick={changeDisplayForward}>&gt;</button>
+				</section>
+			);
+		}
+	}
+}
+
+function infoDisplay(
+	info,
+	currentDisplay,
+	index,
+	changeDisplayBackwards,
+	changeDisplayForward,
+	x
+) {
+	if (currentDisplay === index) {
+		return null;
+	}
+
+	if (x === 0) {
+		return (
+			<section className="CallToActionInactiveF">
+				<button onClick={changeDisplayBackwards}>&lt;</button>
+				<div className="CallToActionInfo">
+					<h3>{info.title}</h3>
+					<p>{info.description}</p>
+					<button className="ContactBtn">{info.contact}</button>
+				</div>
+				<button onClick={changeDisplayForward}>&gt;</button>
+			</section>
+		);
+	} else {
+		return (
+			<section className="CallToActionInactiveB">
+				<button onClick={changeDisplayBackwards}>&lt;</button>
+				<div className="CallToActionInfo">
+					<h3>{info.title}</h3>
+					<p>{info.description}</p>
+					<button className="ContactBtn">{info.contact}</button>
+				</div>
+				<button onClick={changeDisplayForward}>&gt;</button>
+			</section>
+		);
+	}
 }
 
 export default CallToAction;
