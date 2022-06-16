1469286277706cfc6aee5642c4d138bcfed164a0
Louis Doan <louisdoan9@gmail.com>
2022-06-15 20:59:36 -0400

    Refactor code

--- src/components/Header/HeaderDropdown.js
+++ src/components/Header/HeaderDropdown.js
@@ -6,109 +6,61 @@ import { auth } from '../../Firebase';
 
 function HeaderDropDown({ currentPage, setCurrentPage }) {
 	const [user] = useAuthState(auth);
-
 	function signIn() {
 		const provider = new firebase.auth.GoogleAuthProvider();
 		auth.signInWithPopup(provider);
 	}
 
-	const buttons = [
-		<Link to="/">
-			<button
-				onClick={() => {
-					setDisplay('active');
-				}}
-			>
-				HOME
-			</button>
-		</Link>,
-		<Link to="post-a-property">
-			<button
-				onClick={() => {
-					setDisplay('active');
-				}}
-			>
-				POST AD
-			</button>
-		</Link>,
-		<Link to="available-properties">
-			<button
-				onClick={() => {
-					setDisplay('active');
-				}}
-			>
-				AVAILABLE PROPERTIES
-			</button>
-		</Link>,
-	];
+	const [display, setDisplay] = useState(false);
 
-	const [display, setDisplay] = useState('inactive');
-	let firstButton;
-	const [buttonStatus, setButtonStatus] = useState(false);
-
+	let headerTop;
+	/* prettier-ignore */
 	if (currentPage === 'HOME') {
-		firstButton = buttons[0];
-	}
-	if (currentPage === 'POST AD') {
-		firstButton = buttons[1];
-	}
-	if (currentPage === 'AVAILABLE PROPERTIES') {
-		firstButton = buttons[2];
-	}
-
-	let x;
-	if (display === 'active') {
-		x = 'inactive';
-	} else {
-		x = 'active';
+		headerTop = (
+			<Link to="/">
+				<button onClick={() => {setDisplay(true);}}>HOME</button>
+			</Link>
+		);
+	} else if (currentPage === 'POST AD') {
+		headerTop = (
+			<Link to="post-a-property">
+				<button onClick={() => {setDisplay(true);}}>POST AD</button>
+			</Link>
+		);
+	} else if (currentPage === 'AVAILABLE PROPERTIES') {
+		headerTop = (
+			<Link to="available-properties">
+				<button onClick={() => {setDisplay(true);}}>AVAILABLE PROPERTIES</button>
+			</Link>
+		);
 	}
 
 	return (
-		<nav
-			className="HeaderDropdown HeaderNav"
-			disabled={buttonStatus}
-			onClick={() => {
-				setDisplay(x);
-			}}
-		>
-			{firstButton}
-			<div className={`dropdownItems${display}`}>
+		/* prettier-ignore */
+		<nav className="HeaderDropdown HeaderNav" onClick={() => {setDisplay(!display);}}>
+			{headerTop}
+
+			<div className={`dropdownActive-${display}`}>
 				<Link to="/">
-					<button
-						onClick={() => {
-							setCurrentPage('HOME');
-						}}
-					>
-						HOME
-					</button>
+					<button onClick={() => {setCurrentPage('HOME');}}>HOME</button>
 				</Link>
 				<Link to="post-a-property">
-					<button
-						onClick={() => {
-							setCurrentPage('POST AD');
-						}}
-					>
-						POST AD
-					</button>
+					<button onClick={() => {setCurrentPage('POST AD');}}>POST AD</button>
 				</Link>
 				<Link to="available-properties">
-					<button
-						onClick={() => {
-							setCurrentPage('AVAILABLE PROPERTIES');
-						}}
-					>
+					<button onClick={() => {setCurrentPage('AVAILABLE PROPERTIES');}}>
 						AVAILABLE PROPERTIES
 					</button>
 				</Link>
+				
 				{user === null ? (
 					<button className="loginBtn" onClick={signIn}>
 						<span className="headerText">LOGIN</span>
 					</button>
 				) : (
-					/* prettier-ignore */
 					<button className="loginBtn" onClick={() => {auth.signOut();}}>
 							<span className="headerText">LOGOUT</span>
-						</button>
+					</button>
 				)}
 			</div>
 		</nav>
