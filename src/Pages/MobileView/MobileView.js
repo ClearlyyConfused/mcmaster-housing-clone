05b6a4a6ad318e7a1ef428334deb3cff462452f6
Louis Doan <louisdoan9@gmail.com>
2022-07-30 21:55:11 -0400

    Fix iframe display link

--- src/Pages/MobileView/MobileView.js
+++ src/Pages/MobileView/MobileView.js
@@ -7,18 +7,21 @@ function MobileView() {
 	return viewWidth > 500 ? (
 		<div className="mobile-view-page">
 			<iframe
+				title="Large mobile display of website"
 				className="iframeLarge"
-				src="http://localhost:3000/"
+				src="https://offcampus-mcmaster.web.app/"
 				frameborder="0"
 			></iframe>
 			<iframe
+				title="Medium mobile display of website"
 				className="iframeMedium"
-				src="http://localhost:3000/"
+				src="https://offcampus-mcmaster.web.app/"
 				frameborder="0"
 			></iframe>
 			<iframe
+				title="Small mobile display of website"
 				className="iframeSmall"
-				src="http://localhost:3000/"
+				src="https://offcampus-mcmaster.web.app/"
 				frameborder="0"
 			></iframe>
 		</div>
