3a0c18305d07a5d3e449cefed5fbe9f0d224b930
Louis Doan <louisdoan9@gmail.com>
2023-06-16 21:02:20 -0400

    Fix nav highlight sometimes not displaying

--- src/components/Header/HeaderNavBtn.js
+++ src/components/Header/HeaderNavBtn.js
@@ -1,13 +1,24 @@
-function HeaderNavBtn({ buttonName, setCurrentPage, currentPage }) {
+function HeaderNavBtn({ buttonName, setCurrentPage }) {
+	let page = ' ';
+	if (buttonName === 'AVAILABLE PROPERTIES') {
+		page = 'available-properties';
+	} else if (buttonName === 'POST AD') {
+		page = 'post-a-property';
+	}
+
 	return (
 		<button
 			onClick={() => {
 				setCurrentPage(buttonName);
 			}}
 		>
-			{currentPage === buttonName && <div className="pageIndicator"></div>}
+			{/* if button is the HOME button and the current url is the homepage, display the highlight */}
+			{/* else, check if url includes the corresponding text for that button */}
+			{((window.location.href === 'http://localhost:3000/' && buttonName === 'HOME') ||
+				window.location.href.includes(page)) && <div className="pageIndicator"></div>}
 			<span className="headerText">{buttonName}</span>
-			{currentPage === buttonName && <div className="pageIndicator"></div>}
+			{((window.location.href === 'http://localhost:3000/' && buttonName === 'HOME') ||
+				window.location.href.includes(page)) && <div className="pageIndicator"></div>}
 		</button>
 	);
 }
