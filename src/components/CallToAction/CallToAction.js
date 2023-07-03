a645f2556c99619a86bb72306a6f87241f437204
Louis Doan <louisdoan9@gmail.com>
2023-07-03 15:14:05 -0400

    Refactor call to action

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -1,29 +1,33 @@
 import { useState, useEffect } from 'react';
-import { callToActionInfo } from '../../Data';
 import './callToAction.css';
 
+// creates call to action banner on home page
 function CallToAction() {
-	const [currentDisplay, setCurrentDisplay] = useState(0);
+	const [displayedInfo, setDisplayedInfo] = useState(0); // value = index in callToActionInfo to display
 	const [displayDirection, setDisplayDirection] = useState('forward');
 
-	function changeDisplayForward() {
-		setDisplayDirection('forward');
-		if (currentDisplay + 1 === callToActionInfo.length) {
-			setCurrentDisplay(0);
-		} else {
-			setCurrentDisplay(currentDisplay + 1);
-		}
-	}
+	const callToActionInfo = [
+		{
+			title: 'HELP SUPPORT UKRAINIAN STUDENTS AND SCHOLARS',
+			description:
+				'The Centre for Advanced Research in Experimental and Applied Linguistics (ARiEAL) is looking for landlords willing to offer discounted housing for Ukrainian students and scholars coming to McMaster this spring.',
+			contact: 'CONTACT ARIEAL',
+		},
+		{
+			title: 'CITY LAUNCHES RENTAL HOUSING LICENSING PILOT PROGRAM',
+			description:
+				'Requirements, including the program’s phased-in approach, supporting documents, applicable fees and process, are located on the City’s Rental Housing Licensing pilot program website.',
+			contact: 'LEARN MORE',
+		},
+		{
+			title: 'BE A GOOD NEIGHBOUR CAMPAIGN',
+			description:
+				'This fall, McMaster’s Good Neighbour campaign is welcoming students to the surrounding community and encourage positive neighbourly interactions.',
+			contact: 'LEARN MORE',
+		},
+	];
 
-	function changeDisplayBackwards() {
-		setDisplayDirection('backwards');
-		if (currentDisplay - 1 === -1) {
-			setCurrentDisplay(callToActionInfo.length - 1);
-		} else {
-			setCurrentDisplay(currentDisplay - 1);
-		}
-	}
-
+	// automatically cycles through call to action banner every 10 seconds
 	useEffect(() => {
 		const timer = setInterval(changeDisplayForward, 10000);
 		return () => {
@@ -31,12 +35,32 @@ function CallToAction() {
 		};
 	});
 
+	// sets direction animation will go in
+	// sets info index to be displayed
+	function changeDisplayForward() {
+		setDisplayDirection('forward');
+		if (displayedInfo + 1 === callToActionInfo.length) {
+			setDisplayedInfo(0);
+		} else {
+			setDisplayedInfo(displayedInfo + 1);
+		}
+	}
+	function changeDisplayBackwards() {
+		setDisplayDirection('backwards');
+		if (displayedInfo - 1 === -1) {
+			setDisplayedInfo(callToActionInfo.length - 1);
+		} else {
+			setDisplayedInfo(displayedInfo - 1);
+		}
+	}
+
 	return (
 		<div className="call-to-action-container">
+			{/* calls below function on callToActionInfo */}
 			{callToActionInfo.map((info, index) => {
 				return infoDisplay(
 					info,
-					currentDisplay,
+					displayedInfo,
 					index,
 					changeDisplayBackwards,
 					changeDisplayForward,
@@ -47,20 +71,23 @@ function CallToAction() {
 	);
 }
 
+// creates the call to action banner with info
+// either displays info with correct animation or hide info based on which info is being displayed
 function infoDisplay(
 	info,
-	currentDisplay,
+	displayedInfo,
 	index,
 	changeDisplayBackwards,
 	changeDisplayForward,
 	displayDirection
 ) {
+	// if function called with the current info to be displayed, give it class with animation based on direction
+	// else, give it an inactive class to hide it
 	let classVer;
-	if (currentDisplay === index) {
+	if (displayedInfo === index) {
 		classVer = displayDirection === 'forward' ? 'CallToActionF' : 'CallToActionB';
 	} else {
-		classVer =
-			displayDirection === 'forward' ? 'CallToActionInactiveF' : 'CallToActionInactiveB';
+		classVer = displayDirection === 'forward' ? 'CallToActionInactiveF' : 'CallToActionInactiveB';
 	}
 
 	return (
--- src/components/Header/HeaderNavBtn.js
+++ src/components/Header/HeaderNavBtn.js
@@ -14,10 +14,10 @@ function HeaderNavBtn({ buttonName, setCurrentPage }) {
 		>
 			{/* if button is the HOME button and the current url is the homepage, display the highlight */}
 			{/* else, check if url includes the corresponding text for that button */}
-			{((window.location.href === 'http://localhost:3000/' && buttonName === 'HOME') ||
+			{((window.location.href === 'https://offcampus-mcmaster-clone.netlify.app/' && buttonName === 'HOME') ||
 				window.location.href.includes(page)) && <div className="pageIndicator"></div>}
 			<span className="headerText">{buttonName}</span>
-			{((window.location.href === 'http://localhost:3000/' && buttonName === 'HOME') ||
+			{((window.location.href === 'https://offcampus-mcmaster-clone.netlify.app/' && buttonName === 'HOME') ||
 				window.location.href.includes(page)) && <div className="pageIndicator"></div>}
 		</button>
 	);
